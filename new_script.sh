8157a81d0fe3cffaec2985a4b8f516b56c4db608
Ilya Krasnov <ilya.krasnov@gmail.com>
2016-06-15 14:14:49 +0200

    Adds description prompt to script creator

--- new_script.sh
+++ new_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+######################################################################################
+# Created by Ilya Krasnov on 2016/06/15
+# Creates a template of a new script with author and description
+######################################################################################
+
 SCRIPT=${1}.sh
 AUTHOR="Ilya Krasnov"
 TIMESTAMP=`date +%Y/%m/%d`
@@ -8,10 +13,16 @@ then
   echo "${SCRIPT} already exists."
   exit 1
 else
+  echo -n 'Enter description: '
+  read DESCRIPTION
   touch $SCRIPT
   echo "#!/bin/bash" >> $SCRIPT
-  printf "# created by ${AUTHOR} on ${TIMESTAMP}\n" >> $SCRIPT
+  printf "######################################################################################\n" >> $SCRIPT
+  printf "# Created by ${AUTHOR} on ${TIMESTAMP}\n" >> $SCRIPT
+  printf "# ${DESCRIPTION}\n" >> $SCRIPT
+  printf "######################################################################################\n\n" >> $SCRIPT
 
   chmod 775 $SCRIPT
+  exit 0
 fi
 
