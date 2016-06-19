0830c6a6f9378dcc97507deabd590774298293b5
Ilya Krasnov <ilya.krasnov@gmail.com>
2016-06-19 12:30:47 +0200

    Refactors line to file

--- daylog.sh
+++ daylog.sh
@@ -8,26 +8,30 @@ FILE=`date +%Y-%m`.md
 TODAY=`date +%F`
 MONTH_TITLE=`date +'%B %Y'`
 
-# Creates md file for a given month
+function line_to_file {
+  echo "- $1" >> ${FILE}
+}
+
+# Creates monthly file if needed
 if [ ! -e ${FILE} ]
 then
   touch ${FILE}
   echo "# ${MONTH_TITLE}" >> ${FILE}
 fi
 
-# Check if there is already an entry for a given date
+# Adds entries new days or appends to existing entry
 if grep -q "${TODAY}" ${FILE}
 then
   while read line
   do
     `gsed -i '/${TODAY}/a $line' ${FILE}`
-  echo "- $line" >> ${FILE}
-  done #< "${1:-/dev/stdin}"
+    line_to_file
+  done
 else
   printf "\n## ${TODAY} \n" >> ${FILE}
   while read line
   do
-    echo "- $line" >> ${FILE}
-  done #< "${1:-/dev/stdin}"
+    line_to_file
+  done
 fi
 
